552fc27b5ddba82f2bf0ebdfcf25a7ed6ee069bd
ClaytonDewey <clay@dryadmedia.com>
2023-06-09 11:14:39 -0500

    Update ReactDOM render & import styles

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import { App } from './app/App.js';
+import './index.css';
 // Import the store here.
 import { store } from './app/store.js';
 
 // Pass state and dispatch props to the <App /> component.
-const render = () => {
-  ReactDOM.render(
-    <App state={store.getState()} dispatch={store.dispatch} />,
-    document.getElementById('root')
-  );
-};
-render();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App state={store.getState()} dispatch={store.dispatch} />
+  </React.StrictMode>
+);
 
 // Subscribe render to the store.
